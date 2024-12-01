38026a161321f0d1e866618bc488cee42c35f145
Mikhail Gerasimov <dorrrke@yandex.ru>
2024-12-01 14:04:56 +0300

    add db storage + migrate

--- cmd/gt4bookly/main.go
+++ cmd/gt4bookly/main.go
@@ -6,12 +6,18 @@ import (
 	"github.com/Dorrrke/gt4-bookly/internal/server"
 	"github.com/Dorrrke/gt4-bookly/internal/service"
 	"github.com/Dorrrke/gt4-bookly/internal/storage"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func main() {
 	cfg := config.ReadConfig()
 	log := logger.Get(cfg.Debug)
 
+	err := storage.Migrations("postgres://user:password@localhost:5432/gt4?sslmode=disable", "migrations")
+	if err != nil {
+		log.Fatal().Err(err).Send()
+	}
 	stor := storage.New()
 	userService := service.NewUserService(stor)
 	bookService := service.NewBookService(stor)
--- internal/storage/storageerror/errors.go
+++ internal/storage/storageerror/errors.go
@@ -6,4 +6,8 @@ var (
 	ErrBookAlredyExist = errors.New("book alredy exist")
 	ErrEmptyStorage    = errors.New("book storage is empty")
 	ErrBookNoFound     = errors.New("book not found")
+
+	ErrUserAlredyExist = errors.New("user alredy exist")
+	ErrInvalidPassword = errors.New("invalid password")
+	ErrUserNoExist     = errors.New("user no exist")
 )
